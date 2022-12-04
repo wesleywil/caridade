1fb8befe335fa44a7bb8e5078e796602fd4d0a7c
wesleywil <wesleywilsonti@gmail.com>
2022-12-04 16:44:21 -0300

    adding tailwindcss and removing some unused dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 5000,
+    host: true,
+  },
+  integrations: [tailwind()],
+});
